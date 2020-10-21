ea6aaa0ab0099fbed7ce7b9adc59f24a0ec44893
Yu-Ikeda222 <yu.like222@gmail.com>
2020-10-21 02:42:51 +0000

    yuyu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>hgjfkdsll</title>
+        <title>確認from池田</title>
 
 
         <!-- Fonts -->
