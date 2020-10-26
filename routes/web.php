89d999fcd1e92ca7cf45291b67e751d154780971
xxyamatonxx <xxyamatonxx@gmail.com>
2020-10-26 12:11:51 +0000

    projectController作った！これから作業本気大和

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/mypage', 'MypageController@show')->name('mypage.show');
+
+Route::resource('projects', 'ProjectController');
