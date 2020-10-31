f384acc54688c4b4c8d6876571c64a4142451125
xxyamatonxx <xxyamatonxx@gmail.com>
2020-10-31 08:47:49 +0000

    サポーターと金

--- app/Http/Controllers/SupportController.php
+++ app/Http/Controllers/SupportController.php
@@ -14,16 +14,31 @@ class SupportController extends Controller
     {
         $reward = Reward::find($id);
         $project_id = $reward->project->id;
+
         Support::create([
             'user_id' => Auth::id(),
             'project_id' => $project_id,
             'reward_id' => $id,
         ]);
-        // $reward = Reward::find($id);
-        // $project_id = $reward->project->id;
-        // $project = Project::find($project_id);
-        // $project->now_support_money =
-        // $project->save();
+        $projects = Support::where('project_id', $project_id)->get();
+        foreach ($projects as $project) {
+            $price = $project->reward->price;
+            $prices[] = $price;
+        }
+        $sum_price = array_sum($prices);
+
+        foreach ($projects as $project) {
+            $user = $project->user->id;
+            $users[] = $user;
+        }
+        $users_unique = array_unique($users);
+        $supportors = count($users_unique);
+
+        $project = Project::find($project_id);
+        $project->now_support_money = $sum_price;
+        $project->now_supportors = $supportors;
+        $project->save();
+        
         return view('thank_you');
     }
 }
