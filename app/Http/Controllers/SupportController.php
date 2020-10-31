00989eb6e39c7cac5c332e02e545cd47937fd580
xxyamatonxx <xxyamatonxx@gmail.com>
2020-10-31 05:44:45 +0000

    model 追記

--- app/Http/Controllers/SupportController.php
+++ app/Http/Controllers/SupportController.php
@@ -4,16 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Support;
+use App\Reward;
+use App\Project;
 use Illuminate\Support\Facades\Auth;
 
 class SupportController extends Controller
 {
     public function store($id)
     {
+        $reward = Reward::find($id);
+        $project_id = $reward->project->id;
         Support::create([
             'user_id' => Auth::id(),
-            'reward_id' => $id
+            'project_id' => $project_id,
+            'reward_id' => $id,
         ]);
+        // $reward = Reward::find($id);
+        // $project_id = $reward->project->id;
+        // $project = Project::find($project_id);
+        // $project->now_support_money =
+        // $project->save();
         return view('thank_you');
     }
 }
